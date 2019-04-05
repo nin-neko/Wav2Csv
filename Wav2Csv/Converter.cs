45c43a81c5f06494c93fb9e27028c5aff92d80e3
y105109 <tyan-aqua@hotmail.co.jp>
2019-04-05 23:35:58 +0900

    add views and viewmodels

---
+++ Wav2Csv/Converter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Threading.Channels;
+
+using Prism.Mvvm;
+
+
+namespace Wav2Csv
+{
+    class Converter : BindableBase, IConverter
+    {
+        private readonly Channel<string> converter = Channel.CreateUnbounded<string>(new UnboundedChannelOptions
+        {
+            SingleReader = true,
+            SingleWriter = false,
+        });
+
+        public Converter()
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var reader = converter.Reader;
+                    while (await reader.WaitToReadAsync())
+                    {
+                        while (reader.TryRead(out var filePath))
+                        {
+                            this.Converting = true;
+                            try
+                            {
+                                for (int i = 0; i < 10; i++)
+                                {
+                                    await Task.Delay(100);
+                                    this.RaiseProgressChanged(i * 10);
+                                }
+                                
+                            }
+                            finally
+                            {
+                                this.Converting = false;
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        private bool converting;
+        public bool Converting
+        {
+            get => converting;
+            private set => this.SetProperty(ref converting, value);
+        }
+
+        public void BeginConversion(string sourceFilePath)
+        {
+            converter.Writer.TryWrite(sourceFilePath);
+        }
+
+        public event EventHandler<ProgressEventArgs> ProgressChanged;
+
+        private void RaiseProgressChanged(int progress)
+            => this.ProgressChanged?.Invoke(this, new ProgressEventArgs(progress));
+    }
+}
---
+++ Wav2Csv/IConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+
+namespace Wav2Csv
+{
+    interface IConverter : INotifyPropertyChanged
+    {
+        bool Converting { get; }
+
+        void BeginConversion(string sourceFilePath);
+
+        event EventHandler<ProgressEventArgs> ProgressChanged;
+    }
+
+    class ProgressEventArgs : EventArgs
+    {
+        public ProgressEventArgs(int progress)
+        {
+            this.Progress = progress;
+        }
+
+        public int Progress { get; }
+    }
+}
